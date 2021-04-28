2bafd32cf5d97aada1ccee5941a261c754ad06e3
kangseonghee <59370701+kkxxh@users.noreply.github.com>
2021-04-28 10:08:26 +0900

    ruby-on-rails

---
+++ 1-22.rb
@@ -0,0 +1,16 @@
+# 회원 가입 기능과 그 외의 부가 기능을 한번에 구현하는 devise gem
+
+=begin
+gem은 rails에서 필요한 일정 기능을 모아둔 세트박스
+
+구글에 gem device 검색 ->github에 자세한 설명
+
+Gemfile에 
+gem 'devise'
+추가 후
+
+$ bundle install
+
+
+$ rails g devise:install
+$ rails g devise:views
